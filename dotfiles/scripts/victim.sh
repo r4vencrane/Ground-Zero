963d41986db2e82ce5ba045ccfb9c9a4de4278f7
venomcrane <brandon_gachuz20@hotmail.com>
2026-01-09 02:00:10 +0000

    details

--- dotfiles/scripts/victim.sh
+++ dotfiles/scripts/victim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ip_address=$(/bin/cat /home/venomcrane/.config/bin/target | awk '{print $1}')
-machine_name=$(/bin/cat /home/venomcrane/.config/bin/target | awk '{print $2}')
+ip_address=$(/bin/cat /home/$USER/.config/bin/target | awk '{print $1}')
+machine_name=$(/bin/cat /home/$USER/.config/bin/target | awk '{print $2}')
 
 if [ $ip_address ] && [ $machine_name ]; then
     echo "%{F#8dff33}󰓾 %{F#8dff33}$ip_address%{u-} - $machine_name"
